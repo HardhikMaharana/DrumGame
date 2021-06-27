f1b121756762464aeeacdb4626f659a99e18ae26
HardhikMaharana <55658747+HardhikMaharana@users.noreply.github.com>
2021-06-27 12:56:38 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,68 @@
+for(var x=0;x<document.querySelectorAll(".drum").length;x++){
+document.querySelectorAll(".drum")[x].addEventListener("click", function(){
+
+var instrument=this.innerHTML;
+
+allsounds(instrument);
+animation(instrument);
+
+});
+}
+
+document.addEventListener("keydown", function(event){
+  allsounds(event.key);
+  animation(event.key);
+});
+
+function allsounds(key){
+
+  switch(key){
+    case "w":
+    var crash=new Audio("sounds/crash.mp3");
+    crash.play();
+    break;
+
+    case "a":
+    var bass=new Audio("sounds/kick-bass.mp3");
+    bass.play();
+    break;
+
+    case "s":
+    var dhum=new Audio("sounds/snare.mp3");
+    dhum.play();
+    break;
+
+    case "d":
+    var tom1=new Audio("sounds/tom-1.mp3");
+    tom1.play();
+  break;
+
+  case "j":
+  var tom2=new Audio("sounds/tom-2.mp3");
+  tom2.play();
+  break;
+
+  case "k":
+  var tom3=new Audio("sounds/tom-3.mp3");
+  tom3.play();
+  break;
+
+  case "l":
+  var tom4=new Audio("sounds/tom-4.mp3");
+  tom4.play();
+  break;
+
+  default:
+
+  }
+}
+
+function animation(pressedKey){
+  var activeButton=document.querySelector("."+pressedKey);
+
+  activeButton.classList.add("pressed");
+
+  setTimeout(function(){
+    activeButton.classList.remove("pressed");
+  },100);
+}
